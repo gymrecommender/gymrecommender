4ae66c908ab069779a1846cd66da78f28639b17d
egor.shevtsov <Egor.Shevtsov@fer.hr>
2024-11-02 08:49:26 +0100

    Functions handling requests for internal and google API requests are separated

--- frontend/src/services/axios.jsx
+++ frontend/src/services/axios.jsx
@@ -7,7 +7,14 @@ const instance = axios.create({
 	}
 })
 
-const axiosRequest = async (method, endpoint, id = null, data = {}, queryParams={}) => {
+const entityMapping = {
+	'PD': 'place/details/json',
+	'P': 'place/nearbysearch/json',
+	'D': 'directions/json',
+	'G': 'geocode/json'
+}
+
+const axiosInternal = async (method, endpoint, id = null, data = {}, queryParams={}) => {
 	const uri = id ? `/api/${endpoint}/${id}` : `/api/${endpoint}`;
 	const requestConfig = {method, url: uri, data, queryParams}
 	const result = {data: null, error: null}
@@ -22,4 +29,24 @@ const axiosRequest = async (method, endpoint, id = null, data = {}, queryParams=
 	return result;
 }
 
-export default axiosRequest;
+const axiosGoogleAPI = async (entity, queryParams={}) => {
+	const result = {data: null, error: null}
+	if (!entityMapping.hasOwnProperty(entity)) {
+		result.error = "The specified entity is not valid"
+		return result;
+	}
+
+	const uri = `https://maps.googleapis.com/maps/api/${entityMapping[entity]}`;
+	const requestConfig = {method: 'GET', url: uri, queryParams: {...queryParams, key: import.meta.env.VITE_GOOGLE_API_KEY}}
+
+	try {
+		const {data} = await instance(requestConfig);
+		result.data = data;
+	} catch (error) {
+		result.error = errorsParser(error);
+	}
+
+	return result;
+}
+
+export default {axiosInternal, axiosGoogleAPI};
