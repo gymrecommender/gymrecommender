2fd40ae2520d6b08b60f41a1200e73bd109f881c
egor-shevtsov <Egor.Shevtsov@fer.hr>
2024-12-02 14:19:54 +0100

    Vite config is changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,6 @@ dotenv.config();
 export default defineConfig({
 	plugins: [react()],
 	proxy: {
-		'/api': {
-			target: process.env.VITE_BACKEND_URL,
-			changeOrigin: true,
-			secure: false,
-			ws: true
-		},
+		'/api': process.env.VITE_BACKEND_URL
 	},
 });
