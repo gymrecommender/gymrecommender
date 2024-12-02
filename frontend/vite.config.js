e5dd9c38adba9bbcd891fc4f3c01eef932064015
egor-shevtsov <Egor.Shevtsov@fer.hr>
2024-12-02 12:52:02 +0100

    React is served as static site, hence the config is updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,21 +5,10 @@ dotenv.config();
 
 export default defineConfig({
 	plugins: [react()],
-	...(process.env.NODE_ENV === "development" ? {
-		server: {
-			proxy: {
-				'/api': {
-					target: process.env.VITE_BACKEND_URL,
-					changeOrigin: true,
-					secure: false,
-					ws: true,
-				},
-			},
-		}
-	} :
-	{
-		build: {
-			outDir: 'dist',
-		}
-	})
+	proxy: {
+		'/api': {
+			target: process.env.VITE_BACKEND_URL,
+			changeOrigin: true
+		},
+	},
 });
