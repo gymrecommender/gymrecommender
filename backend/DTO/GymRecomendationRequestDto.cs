92bd5c9e6cc64c9de8d86c1f1cd7e66bf72d43d1
Rapirys <ilyarotan@gmail.com>
2025-01-10 22:07:22 +0100

    SCRUM-52, SCRUM-54: Update GymRecommendationRequestDto request dto

--- backend/DTO/GymRecomendationRequestDto.cs
+++ backend/DTO/GymRecomendationRequestDto.cs
@@ -7,7 +7,10 @@ public class GymRecommendationRequestDto
     public string MembershipLength { get; set; } // e.g., "1 month"
     public string? PreferredDepartureTime { get; set; } // Optional, format "hh:mm"
     public string? PreferredArrivalTime { get; set; } // Optional, format "hh:mm"
-    public int MinMembershipPrice { get; set; } // 0 to 100 scale
+    public int MaxMembershipPrice { get; set; } // 0 to 10000 scale
     public int MinOverallRating { get; set; } // 1 to 5 scale
     public int MinCongestionRating { get; set; } // 1 to 5 scale
+    public string City { get; set; }
+    public double Latitude { get; set; } // Latitude of the location
+    public double Longitude { get; set; } // Longitude of the location
 }
